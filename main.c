13f1f17f23a066039f5fb9ac1f2425bd0c5f660b
Gabriel Orrico <gabrielorricoxr@gmail.com>
2026-05-23 22:34:40 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    float numeros[10];
+    float total = 0.0;
+    float media = 0.0;
+
+    for (int i = 0; i < 10; i++) {
+        printf("Digite o %d numero: ", i + 1);
+        scanf("%f", &numeros[i]);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        total = total + numeros[i];
+    }
+
+    media = total / 10.0;
+
+    printf("\n resultado da media final e: %.2f\n\n", media);
+
+    system("pause");
+    return 0;
+}
